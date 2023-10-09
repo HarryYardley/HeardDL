dc51b63ad17c86d246369e5da4bc943bb6e1d17b
Harry <harryy@umich.edu>
2023-10-09 02:07:59 -0400

    modify WA1py.py

--- Whisper_Open_A1/WA1py.py
+++ Whisper_Open_A1/WA1py.py
@@ -6,10 +6,11 @@ result = model.transcribe('harvard.wav', fp16 = False)
 
 text = result['text']
 words = text.split()
-#print(result['text'])
 
 nlp = spacy.load('en_core_web_sm')
 type(nlp)
 doc = nlp(text)
 
 print([t.text for t in doc])
+
+print("Testing github")
